20c5e90732db26651511aa7901e64d539142d6e8
megachonker <>
2022-03-07 18:52:43 +0100

    Ternaire OK + affichage

--- echeck/piece.cc
+++ echeck/piece.cc
@@ -8,28 +8,26 @@ Square Piece::get_pos(){
     return position;
 }
 
-Piece::Piece(std::string nom, Couleur couleur,Square position): nom(nom),couleur(couleur),position(position)
-{
-    //incrémenter l'id de la piece ?
-}
+Piece::Piece(std::string nom, Couleur couleur,Square position): nom(nom),couleur(couleur),position(position){}
 
-Pion::Pion(Couleur couleur,Square position) : Piece(nom = couleur==Blanc ? "\u2659" :"\u265F",couleur,position)
-{
-    to_string();
-}
-
-void Pion::deplace(Square dst){
-
-}
-
-string Piece::to_string() const{
-return  "uid:"+std::to_string(uid)+"\n"+
-        "couleur:"+std::to_string(couleur)+"\n"+
-        "noms:"+nom+"\n"+
-        "position: "+ position.to_string();
+string Piece::to_string() const{    
+return  string("\n")+
+        "noms:      "+nom+"\n"+
+        "position:  "+ position.to_string()+"\n"+
+        "couleur:   "+(couleur==Blanc?"Blanc":"Noir");
 }
 
 void Piece::affiche() const{
     cout << nom ;   
 }
 
+Pion::Pion(Couleur couleur,Square position,bool vierge) : Piece(couleur==Blanc ? "\u2659" :"\u265F",couleur,position),vierge(vierge)
+{cout << to_string()<< endl;}
+
+void Pion::deplace(Square dst){}
+
+
+string Pion::to_string() const{    
+return  Piece::to_string()+"\n"+
+        "vierge:    "+(vierge==true?"Oui":"Non");
+}
--- echeck/piece.hh
+++ echeck/piece.hh
@@ -7,7 +7,7 @@
 class Piece
 {
 protected:
-    short uid;//numeroter de 0 a 32
+    // short uid;//numeroter de 0 a 32
     std::string nom;
     Couleur couleur;
     Square position; // a metre a jour
@@ -25,9 +25,11 @@ public:
 class Pion : public Piece
 {
     private:
+        bool vierge;
         void deplace(Square dst);
     public:
-        Pion(Couleur couleur,Square position);
+        std::string to_string() const;
+        Pion(Couleur couleur,Square position,bool vierge);
 };
 // class Toure : public Piece
 // {}
